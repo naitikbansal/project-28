a85eb22fe72e40b052cafe032bf800c73b8f2d24
naitikbansal <73417177+naitikbansal@users.noreply.github.com>
2020-12-01 12:06:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,85 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+var boy,ground,stone,tree,m1,m2,m3,m4,m5,m6,m7,chain,backgroundImage;
+
+function preload()
+{
+	boy = loadImage("boy.png");
+	tree = loadImage("tree.png");
+	backgroundImage=loadImage("bg.png");
+}
+
+function setup() {
+	createCanvas(1280, 400);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	stone = new Stone(320,225);
+	chain = new SlingShot(stone.body,{x:320 , y:225});
+	m1 = new mango(900,150,10);
+	m2 = new mango(950,120,10);
+	m3 = new mango(1000,100,10);
+	m4 = new mango(950,60,10);
+	m5 = new mango(1050,60,10);
+	m6 = new mango(1100,120,10);
+	ground = Bodies.rectangle(640,385,1290,20,{isStatic:true});
+	World.add(world,ground);
+	
+	Engine.run(engine);
+	  
+}
+function mouseDragged(){
+    Matter.Body.setPosition(stone.body,{x:mouseX,y:mouseY});
+}
+function mouseReleased(){
+    chain.fly();
+}
+
+
+function draw() {
+	if (keyIsPressed === true) {
+		chain.attach();
+	  }
+
+    rectMode(CENTER);
+    background(backgroundImage);
+    
+    push();
+    rect(width/2,400,width,20);
+	  chain.display();
+    drawSprites();
+    console.log(stone);
+    imageMode(CENTER);
+    image(boy,400,300,250,300);
+	image(tree,1000,180,400,400);
+  
+  stone.display();
+	m1.display();
+	m2.display();
+	m3.display();
+	m4.display();
+	m5.display();
+	m6.display();
+  
+  collision(stone,m1);
+	collision(stone,m2);
+	collision(stone,m3);
+	collision(stone,m4);
+	collision(stone,m5);
+	collision(stone,m6);
+
+	
+	
+}
+function collision(a,b){
+	var d = dist(a.body.position.x,a.body.position.y,b.body.position.x,b.body.position.y)
+	if(d <= 50){
+		Body.setStatic(b.body,false);
+	}
+}
